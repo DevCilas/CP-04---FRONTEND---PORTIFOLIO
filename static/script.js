48f6e36de0eda344eeb5dbf9897f79ed5965766e
DevCilas <160146962+DevCilas@users.noreply.github.com>
2025-03-10 23:26:17 -0300

    feat - mobile-section

---
+++ static/script.js
@@ -0,0 +1,12 @@
+// Abre e fehca menu lateral em modo mobile 
+const menuMobile = document.querySelector('.menu-mobile');
+const body = document.querySelector('body');
+
+menuMobile.addEventListener("click", () =>{
+    if(menuMobile.classList.contains("bi-list")){
+        menuMobile.classList.replace("bi-list", "bi-x")
+    }else{
+        menuMobile.classList.replace("bi-x", "bi-list")
+    };
+    body.classList.toggle("menu-nav-active")
+})
